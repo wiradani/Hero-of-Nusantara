ac47a1f4541bb0ff55c1e1327fa72bcb1e456c8e
noerdz <ndzakyp@gmail.com>
2017-08-28 16:00:03 +0700

    Main camera keapus

--- Assets/Framework/Scripts/Framework_GameManager.cs
+++ Assets/Framework/Scripts/Framework_GameManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Framework_GameManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class Framework_GameManager : MonoBehaviour
+{
+    
+    // Use this for initialization
+    void Start()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);	
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 		
-	}
+    }
 }
