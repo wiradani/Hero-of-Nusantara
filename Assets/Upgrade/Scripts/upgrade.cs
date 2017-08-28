d9ad904cf00685b0c56293719acfd76fb23667a1
wiradani <adhan432@gmail.com>
2017-08-28 16:57:13 +0700

    new upgrade

---
+++ Assets/Upgrade/Scripts/upgrade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upgrade : MonoBehaviour {
+
+	public int weapon_id ;
+
+	public void pilihWeapon(){
+		switch (weapon_id) {
+		case 1:
+			Framework_GameManager.playerData.weapon = Weapon.SPEAR;
+			break;
+		
+	
+		}
+	}
+}
