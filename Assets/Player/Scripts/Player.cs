23d9c0a0688ac6009c35639d863f3c8dd3afe174
rielraharj <rielraharj@gmail.com>
2017-08-31 20:48:04 +0700

    asdas

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -3,54 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public int testArea;
-	public float leftTouchArea, velocity, yPoint;
-	Rigidbody2D rb2d;
-	public GameObject hook;
-	public bool isMoving, isReloading;
 	Projectile projectile;
-	public GameObject bullet;
+	public Rigidbody2D rbProjectile;
+	public Rigidbody2D rb;
+	public bool isPressed = false;
+
+
 
 
 	// Use this for initialization
 	void Start () {
-		isReloading = false;
-		isMoving = false;
 		projectile = GameObject.FindObjectOfType<Projectile> ();
-		velocity = 2f;
-		rb2d = GetComponent<Rigidbody2D> ();
-		testArea = 6;
-		leftTouchArea = Screen.width / testArea;
-		yPoint = gameObject.transform.position.y;
+		rbProjectile = projectile.GetComponent<Rigidbody2D> ();
+		rb = GetComponent<Rigidbody2D> ();
 	}
-	
+
+	public float maxDragDistance = 2f;
+
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.position = new Vector2(gameObject.transform.position.x,yPoint);
-		hook.transform.position = new Vector2 (gameObject.transform.position.x+1, gameObject.transform.position.y);
-
-		if (!projectile.isPressed) {
-			if (Input.mousePosition.x < leftTouchArea && Input.GetMouseButton (0)) {
-
-				isMoving = true;
-				Destroy (bullet);
-				Debug.Log ("left");
-				rb2d.velocity = new Vector2 (velocity * -1, rb2d.velocity.y);
-
-
-			} else {
-				isMoving = false;
-				if (Input.mousePosition.x < leftTouchArea&&Input.GetMouseButtonUp (0))
-					isReloading = true;
-
-			}
-		}
-		if (!isMoving && isReloading) {
-			Instantiate (bullet, hook.transform.position, Quaternion.identity);
-			isReloading = false;
+		if (isPressed) {
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if (Vector3.Distance(mousePos, rb.position) > maxDragDistance)
+				rbProjectile.position = rb.position + (mousePos - rb.position).normalized * maxDragDistance;
+			else
+				rbProjectile.position = mousePos;
 		}
 
 
+	}
+
+	void OnMouseDown ()
+	{
+		isPressed = true;
+		rbProjectile.isKinematic = true;
 	}
 
 }
--- Assets/Player/Scripts/Projectile.cs
+++ Assets/Player/Scripts/Projectile.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 	
-	public Rigidbody2D rb;
-	public Rigidbody2D hook;
-	SpringJoint2D sj2d;
-	public float releaseTime = .15f;
-	public float maxDragDistance = 2f;
 
 
-	public bool isPressed = false;
+
 
 	void Start(){
-		sj2d = GetComponent<SpringJoint2D> ();
-		sj2d.connectedBody = hook;
-		gameObject.transform.position = new Vector2 (hook.transform.position.x,hook.transform.position.y);
+
 
 	}
 
@@ -24,41 +17,12 @@ public class Projectile : MonoBehaviour {
 	{
 		
 		
-		if (isPressed)
-		{
-			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-			if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
-				rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
-			else
-				rb.position = mousePos;
-		}
-	}
-
-	void OnMouseDown ()
-	{
-		isPressed = true;
-		rb.isKinematic = true;
-	}
-
-	void OnMouseUp ()
-	{
-		isPressed = false;
-		rb.isKinematic = false;
-
-		StartCoroutine(Release());
-	}
-
-	IEnumerator Release ()
-	{
-		yield return new WaitForSeconds(releaseTime);
-
-		GetComponent<SpringJoint2D>().enabled = false;
-		this.enabled = false;
-
-		yield return new WaitForSeconds(2f);
 
 	}
+
+
+
+
 	void Dead(){
 		Destroy (gameObject);
 	}
