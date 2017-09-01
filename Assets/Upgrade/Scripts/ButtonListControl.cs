8db99268d9c3c1f4b31061f6d673886b05b03c02
noerdz <ndzakyp@gmail.com>
2017-09-01 21:08:47 +0700

    fix upgrade

--- Assets/Upgrade/Scripts/ButtonListControl.cs
+++ Assets/Upgrade/Scripts/ButtonListControl.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class ButtonListControl : MonoBehaviour {
+public class ButtonListControl : MonoBehaviour
+{
 
-	[SerializeField]
-	private GameObject buttonTemplate;
+    [SerializeField]
+    private GameObject buttonTemplate;
 
-	// Use this for initialization
-	void Start () {
-		for(int i=0;i<Framework_GameManager.weaponDatabase.Count;i++){
-			GameObject button = Instantiate (buttonTemplate) as GameObject;
-			button.SetActive (true);
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < Framework_GameManager.weaponDatabase.Count; i++)
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("Upgrade"));
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
 
-			button.GetComponent<ButtonListButton> ().setText (Framework_GameManager.weaponDatabase[i]);
+            button.GetComponent<ButtonListButton>().setText(Framework_GameManager.weaponDatabase[i]);
 
-			button.transform.SetParent (gameObject.transform, false);
+            button.transform.SetParent(gameObject.transform, false);
 			
-		}
-	}
+        }
+    }
 
 }
--- Assets/Upgrade/Scripts/CostumeButtonListControl.cs
+++ Assets/Upgrade/Scripts/CostumeButtonListControl.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class CostumeButtonListControl : MonoBehaviour {
+public class CostumeButtonListControl : MonoBehaviour
+{
 
-	[SerializeField]
-	private GameObject buttonTemplate;
+    [SerializeField]
+    private GameObject buttonTemplate;
 
-	// Use this for initialization
-	void Start () {
-		for(int i=0;i<Framework_GameManager.costumeDatabase.Count;i++){
-			GameObject button = Instantiate (buttonTemplate) as GameObject;
-			button.SetActive (true);
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < Framework_GameManager.costumeDatabase.Count; i++)
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("Upgrade"));
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
 
-			button.GetComponent<CostumeButtonListButton> ().setText (Framework_GameManager.costumeDatabase[i]);
+            button.GetComponent<CostumeButtonListButton>().setText(Framework_GameManager.costumeDatabase[i]);
 
-			button.transform.SetParent (gameObject.transform, false);
+            button.transform.SetParent(gameObject.transform, false);
 
-		}
-	}
+        }
+    }
 }
