5edd19219ef46806fc2bada038b2473b20250962
rielraharj <rielraharj@gmail.com>
2017-09-01 20:06:32 +0700

    up

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	Projectile projectile;
 	public Rigidbody2D rbProjectile;
-	public Rigidbody2D rb;
+
+	public GameObject pointer;
+	public Rigidbody2D rbPointer;
+	//public Rigidbody2D rb;
 	public bool isPressed = false;
 
 
@@ -15,19 +18,23 @@ public class Player : MonoBehaviour {
 	void Start () {
 		projectile = GameObject.FindObjectOfType<Projectile> ();
 		rbProjectile = projectile.GetComponent<Rigidbody2D> ();
-		rb = GetComponent<Rigidbody2D> ();
+		pointer = GameObject.FindGameObjectWithTag ("Pointer");
+		rbPointer = pointer.GetComponent<Rigidbody2D> ();
+		//rb = GetComponent<Rigidbody2D> ();
 	}
+
 
 	public float maxDragDistance = 2f;
 
 	// Update is called once per frame
 	void Update () {
+
 		if (isPressed) {
 			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			if (Vector3.Distance(mousePos, rb.position) > maxDragDistance)
-				rbProjectile.position = rb.position + (mousePos - rb.position).normalized * maxDragDistance;
+			if (Vector3.Distance(mousePos, gameObject.transform.position) > maxDragDistance)
+				rbPointer.position = new Vector2(gameObject.transform.position.x,gameObject.transform.position.y) + (mousePos - new Vector2(gameObject.transform.position.x,gameObject.transform.position.y)).normalized * maxDragDistance;
 			else
-				rbProjectile.position = mousePos;
+				rbPointer.position = mousePos;
 		}
 
 
@@ -35,8 +42,21 @@ public class Player : MonoBehaviour {
 
 	void OnMouseDown ()
 	{
+		
 		isPressed = true;
-		rbProjectile.isKinematic = true;
+		rbPointer.isKinematic = true;
 	}
-
+		
+	public float forceX, forceY, power;
+	public bool isLaunched;
+	void OnMouseUp ()
+	{
+		isPressed = false;
+		rbPointer.isKinematic = false;
+		rbPointer.gravityScale = 1;
+		forceX = gameObject.transform.position.x - rbPointer.position.x;
+		forceY = gameObject.transform.position.y - rbPointer.position.y;
+		rbPointer.AddForce (new Vector2 (forceX*power, forceY*power));
+		isLaunched = true;
+	}
 }
--- Assets/Player/Scripts/Projectile.cs
+++ Assets/Player/Scripts/Projectile.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-	
-
+	Player player;
+	public Vector2 pos, playerPos;
 
 
 
 	void Start(){
+		player = GameObject.FindObjectOfType<Player>();
 
 
 	}
 
 	void Update ()
 	{
-		
+		pos = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
+		playerPos = new Vector2 (player.transform.position.x, player.transform.position.y);
+		if (pos.x > playerPos.x + 20 || pos.y<playerPos.y-10)
+			Dead ();
 		
 
 	}
