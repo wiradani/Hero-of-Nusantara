145660b8529b3a22632d2043d5a2550f2cae3b58
noerdz <ndzakyp@gmail.com>
2017-09-07 22:43:01 +0700

    dialogue Rev

--- Assets/Dialogue/Scripts/Dialogue_Manager.cs
+++ Assets/Dialogue/Scripts/Dialogue_Manager.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class Dialogue_Manager : MonoBehaviour
 {
-    public Text dialogueText, prologueText;
-    public bool isTyping, readyToTheNextDialogue;
-    public bool goToNextPhase;
-    public float typeSpeed = .1f;
+    public Image leftPot, rightPot;
 
+    public GameObject dialogueSet;
+    public Text dialogueText, prologueText, nameText;
+    public float typeSpeed = .1f;
+    public bool skipPhase = false;
     public Image bg;
 
     public string x = "Halo namaku Wira";
@@ -21,25 +22,79 @@ public class Dialogue_Manager : MonoBehaviour
         StartCoroutine(Act1_Lvl1());
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!skipPhase)
+            {
+                skipPhase = true;
+            }
+        }
+    }
+
     IEnumerator TextScroll(string lineOfText, Text _theText)
     {
+        skipPhase = false;
+        dialogueSet.SetActive(false);
+        prologueText.text = "";
+        dialogueText.text = "";
+
         int letter = 0;
         _theText.text = "";
-        isTyping = true;
 
-        while (letter < lineOfText.Length - 1 && isTyping)
+        while (letter < lineOfText.Length - 1 && !skipPhase)
         {
             _theText.text += lineOfText[letter];
             letter++;
             yield return new WaitForSeconds(typeSpeed);
         }
-        isTyping = false;
+
+        yield return null;
         _theText.text = lineOfText;
+        while (!Input.GetMouseButtonDown(0))
+        {
+            yield return null;
+        }
+
+    }
+
+    IEnumerator TextScroll(string _left, string _right, Side _side, string lineOfText, Text _theText)
+    {
+        skipPhase = false;
+        dialogueSet.SetActive(true);
+        prologueText.text = "";
+        dialogueText.text = "";
+
+        if (_side == Side.Left)
+        {
+            //nameText.text = _name;
+        }
+
+        int letter = 0;
+        _theText.text = "";
+
+        while (letter < lineOfText.Length - 1 && !skipPhase)
+        {
+            _theText.text += lineOfText[letter];
+            letter++;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        _theText.text = lineOfText;
+
+        yield return null;
+        while (!Input.GetMouseButtonDown(0))
+        {
+            yield return null;
+        }
     }
 
     IEnumerator SlideshowImage(Sprite _nextImage)
     {
-        goToNextPhase = false;
+        dialogueSet.SetActive(false);
+        prologueText.text = "";
+        dialogueText.text = "";
+
         while (bg.color.a > 0.01f)
         {
             bg.color = Color.Lerp(bg.color, Color.clear, .05f);
@@ -51,56 +106,44 @@ public class Dialogue_Manager : MonoBehaviour
             bg.color = Color.Lerp(bg.color, Color.white, .05f);
             yield return null;
         }
-        goToNextPhase = true;
+
     }
 
     IEnumerator Act1_Lvl1()
     {
-        StartCoroutine(TextScroll("Jakarta 2104, Profesor Dharma seorang jenuis peraih nobel fisika berhasil menciptakan sebuah mesin waktu. Mesin waktu ini berbentuk sebuah gelang bernama “Time Watch” yang dibuatnya bersama dengan asistennya, Arjuna. Profesor berharap dengan adanya mesin waktu ini, ia dapat mengungkap detail sejarah yang telah terlupakan"
+        yield return StartCoroutine(TextScroll("Jakarta 2104, Profesor Dharma seorang jenuis peraih nobel fisika berhasil menciptakan sebuah mesin waktu. Mesin waktu ini berbentuk sebuah gelang bernama “Time Watch” yang dibuatnya bersama dengan asistennya, Arjuna. Profesor berharap dengan adanya mesin waktu ini, ia dapat mengungkap detail sejarah yang telah terlupakan"
             , prologueText));
 
-        while (!Input.GetMouseButtonDown(0))
-        {
-            yield return null;
-        }
-        isTyping = false;
-
-        while (!Input.GetMouseButtonDown(0))
-        {
-            yield return null;
-        }
-
-        prologueText.text = "";
-
-        while (!Input.GetMouseButtonDown(0))
-        {
-            yield return null;
-        }
-
         Sprite nextImage = Resources.Load<Sprite>("Dialog Background/BG Dialog Lab 1 - Early");
-        StartCoroutine(SlideshowImage(nextImage));
+        yield return StartCoroutine(SlideshowImage(nextImage));
 
-        while (!goToNextPhase)
-        {
-            yield return null;
-        }
+        yield return  StartCoroutine(TextScroll("Jakarta 2104,Laboratorium Profsor Dharma"
+            , prologueText));
+
+        yield return StartCoroutine(TextScroll("Arjuna",
+                "Prof,semua persiapan sudah beres,kita bisa memulai uji coba kapapun anda siap"
+            , dialogueText));
+
+        yield return StartCoroutine(TextScroll("Prof Dharma",
+                "Kau yakin Arjuna ingin menjadi tes subjek untuk uji coba ini?Kita bisa menggunakan Hanoman jika kau mau"
+            , dialogueText));
+
+        yield return StartCoroutine(TextScroll("Arjuna",
+                "Saya sangat yakin prof,lagipula jika berhasil gelang ini akan mengubah dunia,dan kita   memerlukan Hanoman sebagai navigator"
+            , dialogueText));
 
         nextImage = Resources.Load<Sprite>("Dialog Background/BG Dialog Lab 2 - Mid");
-        StartCoroutine(SlideshowImage(nextImage));
-
-        while (!goToNextPhase)
-        {
-            yield return null;
-        }
+        yield return StartCoroutine(SlideshowImage(nextImage));
 
         nextImage = Resources.Load<Sprite>("Dialog Background/BG Dialog Lab 3 - Last");
-        StartCoroutine(SlideshowImage(nextImage));
+        yield return StartCoroutine(SlideshowImage(nextImage));
 
-        while (!goToNextPhase)
-        {
-            yield return null;
-        }
-
-        yield return null;
+      
     }
+}
+
+public enum Side
+{
+    Left,
+    Right
 }
