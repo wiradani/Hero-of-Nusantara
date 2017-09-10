3d2bf82109b11e9592e455f5918ddba1a547980d
wiradani <adhan432@gmail.com>
2017-09-10 15:54:04 +0700

    back button

---
+++ Assets/Upgrade/Scripts/back_button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class back_button : MonoBehaviour {
+
+
+	public void  backButton(){
+		Framework_GameManager.instance.BackFromUpgrade();
+	}
+}
